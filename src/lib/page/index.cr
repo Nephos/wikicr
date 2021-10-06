927a8c7da5344ec7a8c1adc4419ecc094a88bf2e
Arthur Poulet <arthur.poulet@sceptique.eu>
2021-10-06 21:32:37 +0200

    index: fix find_by_url with subpath

--- src/lib/page/index.cr
+++ src/lib/page/index.cr
@@ -85,7 +85,8 @@ struct Wikicr::Page
 
     private def find_by_url(text : String, context : Page) : Entry?
       slug_matched = @entries.select { |_, entry|
-        entry.url == Index::Entry.title_to_slug(text)
+        entry.url == Index::Entry.title_to_slug(text) ||
+          File.basename(entry.url) == Index::Entry.title_to_slug(text)
       }.values
       return choose_closer_url(slug_matched, context) unless slug_matched.empty?
       nil
